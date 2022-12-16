54b4752d9ad5d7b76fa3bc9391bb478132689892
Mathew Benson <benson.orina@live.com>
2022-12-16 23:37:07 +0300

    Day 5 Part 1 and 2 Solution

---
+++ day5/part2/solution.cpp
@@ -0,0 +1,136 @@
+#include "input5.hpp"
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <vector>
+
+int main() {
+  std::stringstream inputstringstream{inputdata};
+
+  std::size_t line_counter{0};
+  const std::size_t number_of_stacks{9};
+  const std::size_t data_line_end{8};
+  const std::size_t move_line_length{20};
+  std::size_t stack_location{data_line_end - 1};
+
+  std::array<std::vector<char>, number_of_stacks> crate_containers{};
+
+  auto fill_stacks = [&](const std::string &crate_data) {
+    const auto stride{4};
+
+    for (std::size_t line_index{0}; line_index < data_line_end + 1;
+         ++line_index) {
+      char value = crate_data.substr(1 + (line_index * stride), 1)[0];
+      if (value != ' ') {
+        auto &crate = crate_containers.at(line_index);
+        if (crate.size() < stack_location) {
+          crate.resize(stack_location + 1);
+        }
+        crate.at(stack_location) = value;
+      }
+      std::cout << value << ' ';
+    }
+    stack_location--;
+    std::cout << '\n';
+  };
+
+  auto print_top_values = [&crate_containers] {
+    std::cout << "\nTop Values => ";
+    for (auto &stack : crate_containers) {
+      if (!stack.empty()) {
+        std::cout << stack.back() << ' ';
+      } else {
+        std::cout << "  ";
+      }
+    }
+    std::cout << "\n\n";
+  };
+
+  auto print_stacks = [&crate_containers] {
+    std::cout << "\nStacks => \n";
+
+    std::size_t counter{1};
+    for (auto &stack : crate_containers) {
+      std::cout << "Stack " << counter << " => ";
+      for (auto crate : stack) {
+        std::cout << " | " << crate;
+      }
+      counter++;
+      std::cout << '\n';
+    }
+  };
+
+  auto move_crates = [&crate_containers](const std::string &move_instructions) {
+    std::stringstream movestream{move_instructions};
+
+    std::string movestring;
+    movestream >> movestring;
+    std::size_t number_of_moves{};
+    movestream >> number_of_moves;
+
+    std::cout << '\n' << movestring << ' ' << number_of_moves << ' ';
+
+    std::string from_string{};
+    movestream >> from_string;
+    std::size_t from_index{};
+
+    movestream >> from_index;
+    std::cout << from_string << ' ' << from_index << ' ';
+
+    std::string to_string{};
+    movestream >> to_string;
+    std::size_t to_index{};
+    movestream >> to_index;
+
+    std::cout << to_string << ' ' << to_index << '\n';
+
+    auto &from_container = crate_containers.at(from_index - 1);
+    auto &to_container = crate_containers.at(to_index - 1);
+    std::vector<char> tempcontainer(number_of_moves);
+
+    auto move_crate_values = [](std::vector<char> &from_stack,
+                                std::size_t moves,
+                                std::vector<char> &to_stack) {
+      for (std::size_t index{0}; index < moves; ++index) {
+
+        if (!from_stack.empty()) {
+          auto value = from_stack.back();
+          to_stack.push_back(value);
+          from_stack.pop_back();
+        } else {
+          std::cout << "\t\t\t----> From Stack Empty\n";
+        }
+      }
+    };
+
+    move_crate_values(from_container, number_of_moves, tempcontainer);
+    move_crate_values(tempcontainer, number_of_moves, to_container);
+  };
+
+  while (!inputstringstream.eof()) {
+    std::string linestring{};
+
+    std::getline(inputstringstream, linestring);
+
+    if (linestring.length() == 0) {
+      continue;
+    }
+
+    if (line_counter < data_line_end) {
+      fill_stacks(linestring);
+      line_counter++;
+      continue;
+    }
+
+    if (linestring.length() > move_line_length) {
+      print_top_values();
+      continue;
+    }
+
+    move_crates(linestring);
+    line_counter++;
+    print_stacks();
+    print_top_values();
+  }
+}
