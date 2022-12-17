d7de503750f66eb5f71b106510a0efaf2b60a2d2
Mathew Benson <benson.orina@live.com>
2022-12-17 19:54:51 +0300

    Day 6 Part 1 and 2 Solution Completed

---
+++ day6/part1/solution.cpp
@@ -0,0 +1,37 @@
+#include "input6.hpp"
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <ranges>
+#include <sstream>
+#include <string_view>
+#include <vector>
+
+int main() {
+
+  const auto width{4};
+
+  for (std::size_t index{0}; index < inputdata.length() - 4; ++index) {
+
+    auto endlocation = width + index;
+    auto vals = std::ranges::views::all(inputdata) |
+                std::ranges::views::drop(index) |
+                std::ranges::views::take(width);
+
+    std::string_view store(std::begin(vals), std::end(vals));
+
+    std::cout << store << '\t' << endlocation << '\n';
+
+    std::size_t not_unique{};
+    for (const auto &val : store) {
+      if (store.find(val) != store.rfind(val)) {
+        not_unique++;
+      }
+    }
+
+    if (not_unique == 0) {
+      std::cout << "\t\tFound It\n";
+      break;
+    }
+  }
+}
