15370c3afe20972b218de7bf6991347f1732affc
Mathew Benson <benson.orina@live.com>
2022-12-20 00:40:57 +0300

    Day 8 Part 1 and 2 complete
    - Part 1 Uses for loops and arrays to achieve the results.
    - Part 2 uses C++ ranges.

---
+++ day8/part1/solution.cpp
@@ -0,0 +1,146 @@
+#include "input8.hpp"
+#include <array>
+#include <iostream>
+#include <sstream>
+
+int main() {
+  std::stringstream inputstringstream{inputdata};
+  const auto array_width{99};
+  const auto counter_length{array_width * array_width};
+  std::array<std::array<char, array_width>, array_width> datastore{};
+  std::array<std::array<char, array_width>, array_width> printstore{};
+  std::array<std::size_t, counter_length> visible_tree_count_store{};
+
+  auto get_index_for_array = [](std::size_t row,
+                                std::size_t col) -> std::size_t {
+    return col + (row * array_width);
+  };
+
+  auto print_found = [&](const std::string &name){
+      std::cout<<name<<'\n';
+     for (std::size_t row_index{0}; row_index < array_width; ++row_index) {
+      for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+          std::cout<<printstore.at(row_index).at(col_index);
+      }
+      std::cout << '\n';
+    }
+    std::cout << '\n';
+  };
+
+  auto read_data = [&inputstringstream, &datastore] {
+    for (std::size_t row_index{0}; row_index < array_width; ++row_index) {
+      for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+        char value{};
+        inputstringstream >> value;
+        datastore.at(row_index).at(col_index) = value;
+        std::cout << value;
+      }
+
+      std::cout << '\n';
+    }
+    std::cout << '\n';
+  };
+
+  read_data();
+
+  auto count_trees_left_to_right = [&]() {
+    for (std::size_t row_index{0}; row_index < array_width; ++row_index) {
+      char highest_tree{};
+      char current_tree{};
+      for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+        current_tree = datastore.at(row_index).at(col_index);
+
+        if ((col_index == 0) || (current_tree > highest_tree)) {
+          highest_tree = current_tree;
+          visible_tree_count_store.at(
+              get_index_for_array(row_index, col_index))++;
+          printstore.at(row_index).at(col_index) = current_tree;
+          continue;
+        }
+        printstore.at(row_index).at(col_index) = ' ';
+      }
+    }
+    print_found("Left To Right");
+  };
+
+  count_trees_left_to_right();
+
+  auto count_trees_top_to_bottom = [&]() {
+    for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+      char highest_tree{};
+      char current_tree{};
+      for (std::size_t row_index{0}; row_index < array_width; ++row_index) {
+        current_tree = datastore.at(row_index).at(col_index);
+
+        if ((row_index == 0) || (current_tree > highest_tree)) {
+          highest_tree = current_tree;
+          visible_tree_count_store.at(
+              get_index_for_array(row_index, col_index))++;
+          printstore.at(row_index).at(col_index) = current_tree;
+          continue;
+        }
+        printstore.at(row_index).at(col_index) = ' ';
+      }
+    }
+    print_found("Top To Bottom");
+  };
+
+  count_trees_top_to_bottom();
+
+auto count_trees_right_to_left = [&]() {
+    for (auto row_index{0}; row_index < array_width; ++row_index) {
+      char highest_tree{};
+      char current_tree{};
+      for (auto col_index{array_width-1}; col_index >= 0; --col_index) {
+        auto rowval = static_cast<std::size_t>(row_index);
+        auto colval = static_cast<std::size_t>(col_index);
+        current_tree = datastore.at(rowval).at(colval);
+
+        if ((colval == array_width) || (current_tree > highest_tree)) {
+          highest_tree = current_tree;
+          visible_tree_count_store.at(
+              get_index_for_array(rowval, colval))++;
+          printstore.at(rowval).at(colval) = current_tree;
+          continue;
+        }
+        printstore.at(rowval).at(colval) = ' ';
+      }
+    }
+    print_found("Right To Left");
+  };
+
+  count_trees_right_to_left();
+
+  auto count_trees_bottom_to_top = [&]() {
+    for (auto col_index{0}; col_index < array_width; ++col_index) {
+      char highest_tree{};
+      char current_tree{};
+      for (auto row_index{array_width-1}; row_index >=0 ; --row_index) {
+        auto rowval = static_cast<std::size_t>(row_index);
+        auto colval = static_cast<std::size_t>(col_index);
+        current_tree = datastore.at(rowval).at(colval);
+
+        if ((rowval == array_width) || (current_tree > highest_tree)) {
+          highest_tree = current_tree;
+          visible_tree_count_store.at(
+              get_index_for_array(rowval, colval))++;
+          printstore.at(rowval).at(colval) = current_tree;
+          continue;
+        }
+        printstore.at(rowval).at(colval) = ' ';
+      }
+    }
+    print_found("Bottom To Top");
+  };
+
+  count_trees_bottom_to_top();
+
+  std::size_t visible_tree_count{};
+
+  for (auto &tree : visible_tree_count_store) {
+    if (tree > 0) {
+      visible_tree_count++;
+    }
+  }
+  std::cout << "\nViewable Trees => " << visible_tree_count << '\n';
+}
---
+++ day8/part2/solution.cpp
@@ -0,0 +1,189 @@
+#include "input8.hpp"
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <ranges>
+#include <sstream>
+#include <vector>
+
+int main() {
+  const std::string_view dataview{inputdata};
+  const auto array_width{99};
+
+  constexpr auto delim{'\n'};
+  auto split_view = dataview | std::views::split(delim) | std::views::drop(1) |
+                    std::views::take(array_width);
+  auto reverse_split_view = dataview | std::views::reverse |
+                            std::views::split(delim) | std::views::drop(1) |
+                            std::views::take(array_width);
+  auto joint_view = split_view | std::views::join;
+
+  bool found_one_already{};
+  char current{};
+
+  auto find_trees_less = [&current, &found_one_already](char next) -> bool {
+    if (!found_one_already) {
+      if (next < current) {
+        return true;
+      }
+      if (next == current) {
+        found_one_already = true;
+        return true;
+      }
+    }
+    return false;
+  };
+
+  auto get_left_to_right_view = [&](std::size_t row, std::size_t col) -> auto{
+    auto current_view = joint_view |
+                        std::views::drop((row * array_width) + col) |
+                        std::views::take(array_width - col);
+
+    auto curr = current_view.begin();
+    current = *curr;
+
+    std::vector<char> results{};
+
+    found_one_already = false;
+    auto calculate_view = current_view | std::views::drop(1) |
+                          std::views::take_while(find_trees_less);
+
+    std::ranges::copy(calculate_view, std::back_inserter(results));
+
+    return results;
+  };
+
+  auto get_right_to_left_view = [&](std::size_t row, std::size_t col) -> auto{
+    std::vector<char> results{};
+
+    auto rowviews = split_view | std::views::drop(row);
+
+    auto myview = rowviews.begin();
+    auto current_view =
+        *myview | std::views::reverse | std::views::drop(array_width - col - 1);
+
+    auto curr = current_view.begin();
+    current = *curr;
+
+    found_one_already = false;
+    std::ranges::copy(current_view | std::views::drop(1) |
+                          std::views::take_while(find_trees_less),
+                      std::back_inserter(results));
+    return results;
+  };
+
+  auto get_top_to_bottom_view = [&](std::size_t row, std::size_t col) -> auto{
+    std::vector<char> results{};
+    found_one_already = false;
+    auto rowviews = split_view | std::views::drop(row);
+
+    auto first_selected = *(rowviews.begin()) | std::views::drop(col);
+
+    current = *(first_selected.begin());
+
+    auto remaining_views = rowviews | std::views::drop(1);
+
+    for (auto myrow : remaining_views) {
+      auto selected = myrow | std::views::drop(col);
+
+      auto value = *(selected.begin());
+      if (find_trees_less(value)) {
+        results.push_back(value);
+        continue;
+      } else {
+        break;
+      }
+    }
+
+    return results;
+  };
+
+  auto get_bottom_to_top_view = [&](std::size_t row, std::size_t col) -> auto{
+    std::vector<char> results{};
+    auto rowviews =
+        reverse_split_view | std::views::drop(array_width - (row + 1));
+
+    auto first_selected =
+        *(rowviews.begin()) | std::views::drop(array_width - (col + 1));
+
+    current = *(first_selected.begin());
+    found_one_already = false;
+
+    auto remaining_views = rowviews | std::views::drop(1);
+
+    for (auto myrow : remaining_views) {
+      auto selected = myrow | std::views::drop(array_width - (col + 1));
+
+      auto value = *(selected.begin());
+      if (find_trees_less(value)) {
+        results.push_back(value);
+      } else {
+        break;
+      }
+    }
+
+    return results;
+  };
+
+  auto print_current = [&current] { std::cout << " => " << current << " -> "; };
+
+  std::size_t highest_scenic_score{};
+
+  for (std::size_t row_index{}; row_index < array_width; ++row_index) {
+    for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+
+    std::cout << "Row = " << row_index << "\t| Col = "<<col_index<<'\n';
+
+      std::cout << "Left to Right";
+      auto lines_ltr = get_left_to_right_view(row_index, col_index);
+
+      print_current();
+      for (auto line : lines_ltr) {
+        std::cout << line;
+      }
+      std::cout << " | count = " << lines_ltr.size() << '\n';
+
+      std::cout << "Right to Left";
+      auto lines_rtl = get_right_to_left_view(row_index, col_index);
+
+      print_current();
+      for (auto line : lines_rtl) {
+        std::cout << line;
+      }
+      std::cout << " | count = " << lines_rtl.size() << '\n';
+
+      std::cout << "Top to Bottom";
+
+      auto lines_ttb = get_top_to_bottom_view(row_index, col_index);
+
+      print_current();
+      for (auto line : lines_ttb) {
+        std::cout << line;
+      }
+
+      std::cout << " | count = " << lines_ttb.size() << '\n';
+
+      std::cout << "Bottom to Top";
+
+      auto lines_btt = get_bottom_to_top_view(row_index, col_index);
+
+      print_current();
+      for (auto line : lines_btt) {
+        std::cout << line;
+      }
+
+      std::cout << " | count = " << lines_btt.size() << '\n';
+
+      std::cout << "\n----\n";
+
+      auto scenic_score = lines_ltr.size() * lines_rtl.size() *
+                          lines_ttb.size() * lines_btt.size();
+
+      if (scenic_score > highest_scenic_score) {
+        highest_scenic_score = scenic_score;
+      }
+    }
+    std::cout << '\n';
+  }
+  std::cout << " Highest Scenic Score ==> " << highest_scenic_score << '\n';
+}
