b349b921edaadfa8945d2de848f53b3d828e57c3
Mathew Benson <benson.orina@live.com>
2023-01-08 17:36:38 +0300

    Day 2 update - modernization
    - Use std::string_view literals for input data
    - use std::string_view instead of std::string to ensure no unnecessary
      memory allocation
    - Separation of transformation function from the main function body

--- day2/solution1.cpp
+++ day2/solution1.cpp
@@ -1,59 +1,62 @@
 #include "input2.hpp"
+#include <algorithm>
 #include <iostream>
-#include <map>
-#include <sstream>
+#include <numeric>
+#include <ranges>
 #include <string>
 
-int main() {
-  std::stringstream inputstringstream{inputdata};
+namespace {
+constexpr auto line_delimiter{"\n"sv};
+constexpr auto winning_score{6};
+constexpr auto draw_score{3};
+constexpr auto lose_score{0};
 
-  if (!inputstringstream) {
+constexpr auto rock_score{1};
+constexpr auto paper_score{2};
+constexpr auto scissors_score{3};
+
+const auto mymoves = [](char move) {
+  if (move == 'X') {
+    return 1;
+  }
+  if (move == 'Y') {
     return 2;
   }
+  if (move == 'Z') {
+    return 3;
+  }
+  return 0;
+};
 
-  std::size_t gamescore{};
-
-  while (!inputstringstream.eof()) {
-    std::string gameoutcome{};
-
-    std::size_t roundscore{};
-
-    std::getline(inputstringstream, gameoutcome);
-
-    if (gameoutcome.length() == 0) {
-      continue;
-    }
-
-    char myvalue = gameoutcome[2];
-
-    const auto winning_score{6};
-    const auto draw_score{3};
-
-    const auto rock_score{1};
-    const auto paper_score{2};
-    const auto scissors_score{3};
-
-    std::map<char, std::size_t> mymoves = {
-        {'X', rock_score}, {'Y', paper_score}, {'Z', scissors_score}};
-
-    std::map<std::string, std::size_t> win_lose = {// Same Moves
-                                                   {"A X", draw_score},
-                                                   {"B Y", draw_score},
-                                                   {"C Z", draw_score},
-
-                                                   // I win
-                                                   {"C X", winning_score},
-                                                   {"A Y", winning_score},
-                                                   {"B Z", winning_score}};
-
-    roundscore += mymoves[myvalue];
-    roundscore += win_lose[gameoutcome];
-
-    std::cout << "Game: \n input => " << gameoutcome << "\tScore=> "
-              << roundscore << '\n';
-
-    gamescore += roundscore;
+const auto win_lose = [](std::string_view game) {
+  if (game == "A X"sv || game == "B Y"sv || game == "C Z"sv) {
+    return draw_score;
+  }
+  if (game == "C X"sv || game == "A Y"sv || game == "B Z"sv) {
+    return winning_score;
   }
 
+  return lose_score;
+};
+
+const auto get_game_score = [](const auto &game) {
+  const std::string_view gameoutcome{game.begin(), game.end()};
+  auto roundscore{0};
+
+  char myvalue = gameoutcome.back();
+
+  roundscore += mymoves(myvalue);
+  roundscore += win_lose(gameoutcome);
+
+  return roundscore;
+};
+
+} // namespace
+
+int main() {
+  auto games = inputdata | std::views::split(line_delimiter) |
+               std::views::transform(get_game_score) | std::views::common;
+
+  auto gamescore = std::accumulate(games.begin(), games.end(), 0);
   std::cout << "Games Total Score => " << gamescore << '\n';
 }
--- day2/solution2.cpp
+++ day2/solution2.cpp
@@ -1,65 +1,71 @@
 #include "input2.hpp"
+#include <functional>
 #include <iostream>
 #include <map>
-#include <sstream>
-#include <string>
+#include <numeric>
+#include <ranges>
+
+namespace {
+constexpr auto line_delimiter{"\n"sv};
+
+const auto winning_score{6};
+const auto draw_score{3};
+const auto lose_score{0};
+
+const auto rock_score{1};
+const auto paper_score{2};
+const auto scissors_score{3};
+
+const auto win_draw_lose = [](char move) {
+  if (move == 'Z') {
+    return winning_score;
+  }
+  if (move == 'Y') {
+    return draw_score;
+  }
+  if (move == 'X') {
+    return lose_score;
+  }
+  return 0;
+};
+
+const auto moves = [](std::string_view game) {
+  if (game == "A Z"sv || game == "B Y"sv || game == "C X"sv) {
+    return paper_score;
+  }
+
+  if (game == "B Z"sv || game == "C Y"sv || game == "A X"sv) {
+    return scissors_score;
+  }
+
+  if (game == "C Z"sv || game == "A Y"sv || game == "B X"sv) {
+    return rock_score;
+  }
+
+  return 0;
+};
+
+const auto get_game_score = [](const auto &game) {
+  std::string_view gameoutcome{game.begin(), game.end()};
+
+  auto roundscore{0};
+
+  const auto myvalue = gameoutcome.back();
+
+  roundscore += win_draw_lose(myvalue);
+  roundscore += moves(gameoutcome);
+
+  return roundscore;
+};
+
+} // namespace
 
 int main() {
-  std::stringstream inputstringstream{inputdata};
 
-  if (!inputstringstream) {
-    return 2;
-  }
+  auto games = inputdata | std::views::split(line_delimiter) |
+               std::views::transform(get_game_score) | std::views::common;
 
-  std::size_t gamescore{};
-
-  while (!inputstringstream.eof()) {
-    std::string gameoutcome{};
-
-    std::getline(inputstringstream, gameoutcome);
-
-    std::size_t roundscore{};
-
-    if (gameoutcome.length() == 0) {
-      continue;
-    }
-
-    char myvalue = gameoutcome[2];
-
-    const auto winning_score{6};
-    const auto draw_score{3};
-    const auto lose_score{0};
-
-    std::map<char, std::size_t> win_draw_lose = {
-        {'Z', winning_score}, {'Y', draw_score}, {'X', lose_score}};
-
-    const auto rock_score{1};
-    const auto paper_score{2};
-    const auto scissors_score{3};
-
-    std::map<std::string, std::size_t> moves = {// Win Moves
-                                                {"A Z", paper_score},
-                                                {"B Z", scissors_score},
-                                                {"C Z", rock_score},
-
-                                                // Draw Moves
-                                                {"A Y", rock_score},
-                                                {"B Y", paper_score},
-                                                {"C Y", scissors_score},
-
-                                                // Lose Moves
-                                                {"A X", scissors_score},
-                                                {"B X", rock_score},
-                                                {"C X", paper_score}};
-
-    roundscore += win_draw_lose[myvalue];
-    roundscore += moves[gameoutcome];
-
-    std::cout << "Game: \n input => " << gameoutcome << "\tScore=> "
-              << roundscore << '\n';
-
-    gamescore += roundscore;
-  }
+  const auto gamescore{std::accumulate(games.begin(), games.end(), 0)};
 
   std::cout << "Games Total Score => " << gamescore << '\n';
 }
