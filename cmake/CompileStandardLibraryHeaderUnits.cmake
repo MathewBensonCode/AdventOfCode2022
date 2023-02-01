232d6e64dad5155bd253c5718f1796144fd1d95e
Mathew Benson <benson.orina@live.com>
2023-02-01 15:01:45 +0300

    moved header unit generation to cmake file

--- cmake/CompileStandardLibraryHeaderUnits.cmake
+++ cmake/CompileStandardLibraryHeaderUnits.cmake
@@ -8,16 +8,30 @@ function(CompileStandardLibraryHeaderUnit header)
         execute_process(COMMAND ${CMAKE_CXX_COMPILER} "-std=c++20 --precompile -x c++-system-header ${header}")
 
     elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
-        execute_process(COMMAND ${CMAKE_CXX_COMPILER} "/std:c++latest /exportHeader /headerName:angle ${header}")
+        execute_process(COMMAND ${CMAKE_CXX_COMPILER} /std:c++latest /exportHeader /headerName:angle ${header} OUTPUT_VARIABLE cout)
     endif()
 
-    message("Compiled Header Unit for => ${header}")
+    message("Compiled Header Unit for => ${header} \n ${cout} \n")
 
 endfunction()
 
+function(CreateMsvcCompilerFlags headername)
+target_compile_options(project_options INTERFACE /headerUnit:angle "${headername}=./${headername}.ifc")
+endfunction()
 
-set(SystemHeaders iostream vector memory algorithm iterator string_view numeric)
+
+set(SystemHeaders iostream vector memory algorithm iterator string_view numeric ranges string map charconv array sstream)
 
 foreach(headername ${SystemHeaders})
     CompileStandardLibraryHeaderUnit(${headername})
 endforeach()
+
+#add -fmodule-ts for gcc
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+target_compile_options(project_options INTERFACE -fmodules-ts -flang-info-include-translate)
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+target_compile_options(project_options INTERFACE "/translateInclude")
+    foreach(headername ${SystemHeaders})
+        CreateMsvcCompilerFlags(${headername}) 
+    endforeach()
+endif()
