f99b36c57fa3e2ac4cd77c141256d701ed57cd09
Mathew Benson <benson.orina@live.com>
2024-05-16 21:32:21 +0300

    fix clang-format, day1_solution1 - remove cpp17 line that was incompatible in .clang-format - remove unnecessary includes in day1_solution1

--- day1/solution1.cpp
+++ day1/solution1.cpp
@@ -1,18 +1,15 @@
 #include "input1.hpp"
 #include <algorithm>
-#include <charconv>
-#include <numeric>
 #include <ranges>
 #include <string_view>
 #include <vector>
 
 import day1lib;
 
-int main() {
+int main()
+{
   constexpr auto get_section_data = day1::get_section_sum{};
-  auto sections = std::string_view{day1::input_string} |
-                  std::views::split(day1::section_delimiter) |
-                  std::views::transform(get_section_data);
+  auto sections = std::string_view{ day1::input_string } | std::views::split(day1::section_delimiter) | std::views::transform(get_section_data);
 
   std::vector<int> sums{};
   std::ranges::copy(sections, std::back_inserter(sums));
