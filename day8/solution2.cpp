cc35383014d888c72bd3a61d33e5010346343775
Mathew Benson <benson.orina@live.com>
2024-05-30 19:43:36 +0300

    day 8 upgrade using ranges and views
    - using std::ranges::count_if() to count the relevant trees

--- day8/solution2.cpp
+++ day8/solution2.cpp
@@ -1,185 +1,175 @@
 #include "input8.hpp"
 import std;
 
-int main() {
-  constexpr std::string_view dataview{inputdata};
-  constexpr std::string_view delim{"\n"};
+using namespace std::string_view_literals;
 
-  const auto array_width{99};
+namespace day8
+{
 
-  auto split_view = dataview | std::views::split(delim) | std::views::drop(1) |
-                    std::views::take(array_width);
+const auto array_width{ 99 };
 
-  auto reverse_split_view = dataview | std::views::reverse |
-                            std::views::split(delim) | std::views::drop(1) |
-                            std::views::take(array_width);
-  auto joint_view = split_view | std::views::join;
+auto get_columns(auto &rowview, const auto row, const auto column)
+{
+  return rowview | std::views::drop(row) | std::views::drop(1) | std::views::transform([column](const auto &row) {
+    auto iterator = row.begin();
+    std::advance(iterator, column);
+    return *iterator;
+  }) | std::views::take(array_width-(row+1));
+}
 
-  bool found_one_already{};
-  char current{};
+auto get_reverse_columns(auto &dataview, const std::size_t row, const std::size_t column)
+{
+  return dataview | std::views::reverse | std::views::split("\n"sv) | std::views::drop(1) | std::views::drop(array_width - (row+1))| std::views::transform([column](const auto &row) {
+    auto iterator = row.begin();
+    std::advance(iterator, (array_width - (column + 1)));
+    return *iterator;
+  }) | std::views::take(row-1);
+}
 
-  auto find_trees_less = [&current, &found_one_already](char next) -> bool {
-    if (!found_one_already) {
-      if (next < current) {
-        return true;
-      }
-      if (next == current) {
-        found_one_already = true;
-        return true;
-      }
-    }
-    return false;
-  };
 
-  auto get_left_to_right_view = [&](std::size_t row, std::size_t col) -> auto{
-    auto current_view = joint_view |
-                        std::views::drop((row * array_width) + col) |
-                        std::views::take(array_width - col);
+}// namespace day8
 
-    auto curr = current_view.begin();
-    current = *curr;
+using namespace day8;
 
-    std::vector<char> results{};
-
-    found_one_already = false;
-    auto calculate_view = current_view | std::views::drop(1) |
-                          std::views::take_while(find_trees_less);
-
-    std::ranges::copy(calculate_view, std::back_inserter(results));
-
-    return results;
-  };
-
-  auto get_right_to_left_view = [&](std::size_t row, std::size_t col) -> auto{
-    std::vector<char> results{};
-
-    auto rowviews = split_view | std::views::drop(row);
-
-    auto myview = rowviews.begin();
-    auto current_view =
-        *myview | std::views::reverse | std::views::drop(array_width - col - 1);
-
-    auto curr = current_view.begin();
-    current = *curr;
-
-    found_one_already = false;
-    std::ranges::copy(current_view | std::views::drop(1) |
-                          std::views::take_while(find_trees_less),
-                      std::back_inserter(results));
-    return results;
-  };
-
-  auto get_top_to_bottom_view = [&](std::size_t row, std::size_t col) -> auto{
-    std::vector<char> results{};
-    found_one_already = false;
-    auto rowviews = split_view | std::views::drop(row);
-
-    auto first_selected = *(rowviews.begin()) | std::views::drop(col);
-
-    current = *(first_selected.begin());
-
-    auto remaining_views = rowviews | std::views::drop(1);
-
-    for (auto myrow : remaining_views) {
-      auto selected = myrow | std::views::drop(col);
-
-      auto value = *(selected.begin());
-      if (find_trees_less(value)) {
-        results.push_back(value);
-        continue;
-      }
-      break;
-    }
-
-    return results;
-  };
-
-  auto get_bottom_to_top_view = [&](std::size_t row, std::size_t col) -> auto{
-    std::vector<char> results{};
-    auto rowviews =
-        reverse_split_view | std::views::drop(array_width - (row + 1));
-
-    auto first_selected =
-        *(rowviews.begin()) | std::views::drop(array_width - (col + 1));
-
-    current = *(first_selected.begin());
-    found_one_already = false;
-
-    auto remaining_views = rowviews | std::views::drop(1);
-
-    for (auto myrow : remaining_views) {
-      auto selected = myrow | std::views::drop(array_width - (col + 1));
-
-      auto value = *(selected.begin());
-      if (find_trees_less(value)) {
-        results.push_back(value);
-      } else {
-        break;
-      }
-    }
-
-    return results;
-  };
-
-  auto print_current = [&current] { std::cout << " => " << current << " -> "; };
+int main()
+{
+  auto dataview = std::string_view{ inputdata } | std::views::drop(1);
+  auto rowview = dataview | std::views::split("\n"sv);
+  auto print_current = [](char current) { std::cout << " => " << current << " -> "; };
 
   std::size_t highest_scenic_score{};
 
-  for (std::size_t row_index{}; row_index < array_width; ++row_index) {
-    for (std::size_t col_index{0}; col_index < array_width; ++col_index) {
+  unsigned row_index{};
+    for (const auto &row : rowview) {
+      unsigned col_index{};
+        for (const auto &column : row) {
 
-      std::cout << "Row = " << row_index << "\t| Col = " << col_index << '\n';
+            const auto current = column - '0';
 
-      std::cout << "Left to Right";
-      auto lines_ltr = get_left_to_right_view(row_index, col_index);
+          std::cout << "Row = " << row_index << "\t| Col = " << col_index << '\n';
 
-      print_current();
-      for (auto line : lines_ltr) {
-        std::cout << line;
-      }
-      std::cout << " | count = " << lines_ltr.size() << '\n';
+          std::cout << "Left to Right";
+          bool found_one_already{ false };
+          auto lines_ltr = row | std::views::drop(col_index + 1) | std::views::take_while([&found_one_already, current](const auto next) {
+                  auto nextint = next - '0';
+            if (!found_one_already) {
+                if (nextint < current) {
+                  std::cout << nextint;
+                  return true;
+              }
+                if (nextint == current) {
+                  std::cout << nextint;
+                  found_one_already = true;
+                  return true;
+              }
+          }
+          std::cout << "/" << nextint << "/";
+          return false;
+          });
 
-      std::cout << "Right to Left";
-      auto lines_rtl = get_right_to_left_view(row_index, col_index);
+          print_current(column);
 
-      print_current();
-      for (auto line : lines_rtl) {
-        std::cout << line;
-      }
-      std::cout << " | count = " << lines_rtl.size() << '\n';
+          unsigned ltrsize{};
+          for(const auto& counter: lines_ltr){
+              ltrsize++;
+          }
 
-      std::cout << "Top to Bottom";
+          std::cout << " | count = " << ltrsize << '\n';
 
-      auto lines_ttb = get_top_to_bottom_view(row_index, col_index);
+          std::cout << "Right to Left";
+          found_one_already = false;
+          auto lines_rtl = row | std::views::reverse | std::views::drop(array_width - col_index) | std::views::take_while([&found_one_already, current ](const auto next) {
+                  auto nextint = next - '0';
+            if (!found_one_already) {
+                if (nextint < current) {
+                  std::cout << nextint;
+                  return true;
+              }
+                if (next == current) {
+                  std::cout << nextint;
+                  found_one_already = true;
+                  return true;
+              }
+          }
+          std::cout << "/" << nextint << "/";
+          return false;
+          });
 
-      print_current();
-      for (auto line : lines_ttb) {
-        std::cout << line;
-      }
+          print_current(column);
 
-      std::cout << " | count = " << lines_ttb.size() << '\n';
+          unsigned rtlsize{};
+          for(const auto& counter: lines_rtl){
+              rtlsize++;
+          }
 
-      std::cout << "Bottom to Top";
+          std::cout << " | count = " << rtlsize << '\n';
 
-      auto lines_btt = get_bottom_to_top_view(row_index, col_index);
+          std::cout << "Top to Bottom";
+          found_one_already = false;
+          auto lines_ttb = get_columns(rowview, row_index, col_index) | std::views::take_while([&found_one_already, current](const auto next) {
+            const auto nextint = next - '0';
+            if (!found_one_already) {
+                if (nextint < current) {
+                  std::cout << nextint;
+                  return true;
+              }
+                if (nextint == current) {
+                  std::cout << nextint;
+                  found_one_already = true;
+                  return true;
+              }
+          }
+          std::cout << "/" << nextint << "/";
+          return false;
+          });
 
-      print_current();
-      for (auto line : lines_btt) {
-        std::cout << line;
-      }
+          print_current(column);
 
-      std::cout << " | count = " << lines_btt.size() << '\n';
+          unsigned ttbsize{};
+          for(const auto& counter: lines_ttb){
+              ttbsize++;
+          }
 
-      std::cout << "\n----\n";
+          std::cout << " | count = " << ttbsize << '\n';
 
-      auto scenic_score = lines_ltr.size() * lines_rtl.size() *
-                          lines_ttb.size() * lines_btt.size();
+          std::cout << "Bottom to Top";
 
-      if (scenic_score > highest_scenic_score) {
-        highest_scenic_score = scenic_score;
-      }
+          found_one_already = false;
+          auto lines_btt = get_reverse_columns(dataview, row_index, col_index) | std::views::drop(1) | std::views::take_while([&found_one_already, current](const auto next) {
+            const auto nextint = next - '0';
+            if (!found_one_already) {
+                if (nextint < current) {
+                  std::cout << nextint;
+                  return true;
+              }
+                if (nextint == current) {
+                  std::cout << nextint;
+                  found_one_already = true;
+                  return true;
+              }
+          }
+          std::cout << "/" << nextint << "/";
+          return false;
+          });
+          print_current(column);
+
+          unsigned bttsize{};
+          for(const auto& counter: lines_btt){
+              bttsize++;
+          }
+          std::cout << " | count = " << bttsize << '\n';
+
+          std::cout << "\n----\n";
+
+          auto scenic_score = (ltrsize * rtlsize * ttbsize * bttsize);
+
+            if (scenic_score > highest_scenic_score) {
+              highest_scenic_score = static_cast<std::size_t>(scenic_score);
+          }
+          col_index++;
+        }
+      std::cout << '\n';
+      row_index++;
     }
-    std::cout << '\n';
-  }
   std::cout << " Highest Scenic Score ==> " << highest_scenic_score << '\n';
 }
