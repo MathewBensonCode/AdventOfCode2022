1c7024a6121ca35f8e2d0a0fa26eb96dad713811
Mathew Benson <mathew@benson.co.ke>
2025-02-07 00:27:01 +0300

    day1 fix to ensure consistency with msvc

--- day1/day1lib.cpp
+++ day1/day1lib.cpp
@@ -1,28 +1,35 @@
 export module day1lib;
 import std;
 
-namespace day1 {
-export std::string_view section_delimiter{"\n\n"};
-std::string_view line_delimiter{"\n"};
+namespace day1
+{
 
-struct string_to_int {
-  auto operator()(const auto &line) const{
-    int number{};
-    std::from_chars(line.begin(), line.end(), number);
+struct string_to_int
+{
+  auto operator()(const auto &line) const
+  {
+    unsigned number{};
+    std::string_view line_string{line.begin(), line.end()};
+    std::from_chars(line_string.data(), line_string.data()+line_string.size(), number);
+    std::println("{}", number);
     return number;
   }
 };
 
-export struct get_section_sum {
+export struct get_section_sum
+{
 
-  unsigned operator()(const auto &section) {
-     auto lines = std::views::transform(
-        std::views::split(section, line_delimiter), string_to_int{});
+  unsigned operator()(const auto &section)
+  {
+    std::string_view line_delimiter{ "\n" };
+    auto lines = std::views::transform(
+      std::views::split(section, line_delimiter), string_to_int{});
 
-     auto section_sum =
-        std::accumulate(lines.begin(), lines.end(), unsigned{});
+    auto section_sum =
+      std::accumulate(lines.begin(), lines.end(), unsigned{});
+    std::println("Section Sum => {}\n", section_sum);
     return section_sum;
   }
 };
 
-} // namespace day1
+}// namespace day1
--- day1/solution1.cpp
+++ day1/solution1.cpp
@@ -3,25 +3,12 @@ import std;
 import input1;
 
 using namespace day1;
-
-auto get_highest() {
-
-  auto sections = std::string_view{inputdata} |
-                  std::views::split(section_delimiter) |
-                  std::views::transform(get_section_sum{});
-
-  return *(std::ranges::max_element(sections));
-};
-
-int main() {
-
-  try {
-
-    auto highest = get_highest();
-
-    std::print("Highest Element = {}\n ", highest);
-
-  } catch (std::exception &e) {
-    std::cerr << "Error Occurred" << e.what() << '\n';
+int main()
+{
+    try {
+      auto sections = std::string_view{ inputdata } | std::views::split(std::string_view{"\n\n"}) | std::views::transform(get_section_sum{});
+      std::print("Highest Element = {}\n ", std::ranges::max(sections));
+    } catch (std::exception &e) {
+      std::cerr << "Error Occurred" << e.what() << '\n';
   }
 }
