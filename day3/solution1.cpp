7db766cffd10eea50e8401f5a6cb41c006ee2fd6
Mathew Benson <benson.orina@live.com>
2025-02-10 17:02:06 +0300

    std::size_t to unsigned to remove header requirement

--- day3/solution1.cpp
+++ day3/solution1.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <iostream>
 #include <ranges>
+#include <string_view>
 
 namespace
 {
@@ -11,7 +12,7 @@ const auto capital_letter_offset{ 38 };
 const auto small_capital_boundary{ 27 };
 constexpr std::string_view line_delimiter{ "\n" };
 
-const auto calculate_char = [](std::size_t value) {
+const auto calculate_char = [](unsigned value) {
   if (value < small_capital_boundary) {
     return static_cast<unsigned char>(value + small_letter_offset);
 }
@@ -19,7 +20,7 @@ return static_cast<unsigned char>(value + capital_letter_offset);
 };
 
 const auto calculate_index = [](unsigned char letter) {
-  auto value = static_cast<std::size_t>(letter);
+  auto value = static_cast<unsigned>(letter);
 
     if (value > small_letter_offset) {
       return value - small_letter_offset;
@@ -32,7 +33,7 @@ const auto calculate_index = [](unsigned char letter) {
 int main()
 {
 
-  std::size_t count{};
+  unsigned count{};
 
   auto lines = inputdata | std::views::split(line_delimiter);
 
@@ -41,8 +42,8 @@ int main()
 
       auto halflength = lineinput.size() / 2;
 
-      std::array<std::size_t, compartment_size> first_compartment{};
-      std::array<std::size_t, compartment_size> second_compartment{};
+      std::array<unsigned, compartment_size> first_compartment{};
+      std::array<unsigned, compartment_size> second_compartment{};
 
       auto first_half = lineinput.substr(0, halflength);
       auto second_half = lineinput.substr(halflength);
@@ -52,7 +53,7 @@ int main()
       std::cout << "First_Half => " << first_half << '\n';
       std::cout << "Second_Half => " << second_half << '\n';
 
-        for (std::size_t index = 0; index < halflength; ++index) {
+        for (unsigned index = 0; index < halflength; ++index) {
           const auto first_half_char_index =
             calculate_index(static_cast<unsigned char>(first_half[index]));
           const auto second_half_char_index =
@@ -62,7 +63,7 @@ int main()
           second_compartment.at(second_half_char_index)++;
         }
 
-        for (std::size_t index = 1; index < compartment_size; ++index) {
+        for (unsigned index = 1; index < compartment_size; ++index) {
             if (first_compartment.at(index) > 0 && second_compartment.at(index) > 0) {
               count += index;
               std::cout << "Found character : " << calculate_char(index)
