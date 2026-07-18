0b80b1708fe738c65fb6c0585431a28fa02d1169
benson <benson@benson.co.ke>
2026-07-18 05:00:17 +0300

    cleaner cmake

--- day1/solution2.cpp
+++ day1/solution2.cpp
@@ -1,10 +1,10 @@
 #include "input1.hpp"
 #include <algorithm>
 #include <charconv>
-#include <iostream>
 #include <numeric>
 #include <ranges>
 #include <vector>
+#include <print>
 
 namespace
 {
@@ -15,13 +15,13 @@ const auto get_section_data = [](const auto &section) {
   auto lines =
     section | std::views::split(line_delimiter) | std::views::transform([](const auto &line) {
       std::string_view line_string{ line.begin(), line.end() };
-      int number{};
+      unsigned number{};
       std::from_chars(line_string.data(),
         line_string.data() + line_string.size(),
         number);
       return number;
     });
-  return std::accumulate(lines.begin(), lines.end(), 0);
+  return std::accumulate(lines.begin(), lines.end(), 0U);
 };
 
 }// namespace
@@ -31,11 +31,11 @@ int main()
 
   auto sections = inputdata | std::views::split(section_delimiter) | std::views::transform(get_section_data);
 
-  std::vector<int> results{};
+  std::vector<int> results;
   std::ranges::copy(sections, std::back_inserter(results));
 
   std::ranges::sort(results, std::ranges::greater());
   auto top3 = results | std::views::take(3) | std::views::common;
   auto highest3 = std::accumulate(top3.begin(), top3.end(), 0);
-  std::cout << "Sum of Highest 3 Elements = " << highest3 << '\n';
+  std::println("Sum of Highest 3 Elements = {}", highest3);
 }
